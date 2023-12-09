61ff7b4d5d532ad38aa509f13f51690cf994e02e
mori618 <148190402+mori618@users.noreply.github.com>
2023-12-09 07:44:39 +0000

    12/9

--- rails6-bookers2-debug-ruby3-main/app/models/user.rb
+++ rails6-bookers2-debug-ruby3-main/app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  has_many :followers, class_name: "Relationship", foreign_key: "followeds", dependent: :destroy
+  has_many :followers, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followeds, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :user_follower, through: :followers, source: :followed
   has_many :user_followed, through: :followeds, source: :follower
